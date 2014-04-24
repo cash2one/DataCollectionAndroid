1b7f5c7fad793f2f9f7ef7bd71321bf01c0fe3f4
TomWerner <tomwer3@gmail.com>
2014-04-24 08:29:05 -0500

    Updated layouts to use strings.xml
    No more hardcoded values in layout strings, got rid of lint warnings

--- smsRead/gen/edu/uiowa/datacollection/sms/R.java
+++ smsRead/gen/edu/uiowa/datacollection/sms/R.java
@@ -280,6 +280,7 @@ containing a value of this type.
     }
     public static final class string {
         public static final int action_settings=0x7f07001a;
+        public static final int app_help_button_text=0x7f070026;
         public static final int app_id=0x7f07001b;
         public static final int app_name=0x7f070019;
         public static final int com_facebook_choose_friends=0x7f07000f;
@@ -307,7 +308,17 @@ containing a value of this type.
         public static final int com_facebook_usersettingsfragment_log_in_button=0x7f070008;
         public static final int com_facebook_usersettingsfragment_logged_in=0x7f070009;
         public static final int com_facebook_usersettingsfragment_not_logged_in=0x7f07000a;
-        public static final int hello_world=0x7f07001c;
+        public static final int facebook_login_button_text=0x7f07001d;
+        public static final int get_help_button_text=0x7f070024;
+        public static final int help_button_text=0x7f070020;
+        public static final int launch_survey_button_text=0x7f070023;
+        public static final int phone_label_text=0x7f07001f;
+        public static final int post_registration_text=0x7f070027;
+        public static final int registration_text=0x7f07001c;
+        public static final int report_bullying_button_text=0x7f070025;
+        public static final int reregister_button_text=0x7f070022;
+        public static final int submit_button_text=0x7f070021;
+        public static final int twitter_login_button_text=0x7f07001e;
     }
     public static final class style {
         /** 
